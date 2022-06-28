08f9f2cd0e834822e6707fabb367fee922c3f434
kyllianbou <kyllian.boussaid@gmail.com>
2022-06-28 12:34:02 +0200

    partenaire

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -17,7 +17,7 @@ const Galerie = () => {
                 <h1>Partenaire</h1>
             </div>
             <div className="bod">
-                <p>Pourquoi s’engager avec les réseau des femmes Nigeriennes ?</p>
+                <h2>Pourquoi s’engager avec les réseau des femmes Nigeriennes ?</h2>
                 <ul>
                     <li>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</li>
                     <li>Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</li>
